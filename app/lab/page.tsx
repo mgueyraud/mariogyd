a78ed8284a0d3d5005d41b1ea1e4c2dde0b5ec8f
mgueyraud <mgueyraud.junior@gmail.com>
2024-06-07 11:47:18 -0400

    Added animation for lab and index page

--- app/lab/page.tsx
+++ app/lab/page.tsx
@@ -1,24 +1,40 @@
-import Link from 'next/link'
-import { getLabPosts } from './fetchers';
-import LabVideo from '@/components/custom/LabVideo';
+import Link from "next/link";
+import { getLabPosts } from "./fetchers";
+import LabVideo from "@/components/custom/LabVideo";
+import { CSSProperties } from "react";
 
 export default async function Lab() {
-
-    const posts = await getLabPosts();
+  const posts = await getLabPosts();
 
   return (
     <>
-        <h1 className="font-semibold">Lab</h1>
-        <p className="font-light mt-2 text-gray-300">
-            A creative hub for UI experiments, component explorations, and interaction design. Discover innovative ideas and cutting-edge techniques that push the boundaries of digital experiences.
-        </p>
-        <div className='mt-6 grid grid-cols-1 gap-5 md:grid-cols-2'>
-            {posts.map(post => (
-                <Link key={post.slug} href={`lab/${post.slug}`} aria-label={post.frontmatter.title} className='block aspect-square focus-visible:outline-1 focus-visible:outline-offset-4 focus-visible:outline-white'>
-                    <LabVideo src={post.frontmatter.video} />
-                </Link>
-            ))}
-        </div>
+      <h1
+        className="font-semibold animate-enter"
+        style={{ "--stagger": 1 } as CSSProperties}
+      >
+        Lab
+      </h1>
+      <p
+        className="font-light mt-2 text-gray-300 animate-enter"
+        style={{ "--stagger": 2 } as CSSProperties}
+      >
+        A creative hub for UI experiments, component explorations, and
+        interaction design. Discover innovative ideas and cutting-edge
+        techniques that push the boundaries of digital experiences.
+      </p>
+      <div className="mt-6 grid grid-cols-1 gap-5 md:grid-cols-2">
+        {posts.map((post, i) => (
+          <Link
+            key={post.slug}
+            href={`lab/${post.slug}`}
+            aria-label={post.frontmatter.title}
+            className="block aspect-square focus-visible:outline-1 focus-visible:outline-offset-4 focus-visible:outline-white animate-enter"
+            style={{ "--stagger": i + 3 } as CSSProperties}
+          >
+            <LabVideo src={post.frontmatter.video} />
+          </Link>
+        ))}
+      </div>
     </>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,56 @@
 import Link from "next/link";
+import { CSSProperties } from "react";
 
 export default function Home() {
   return (
     <>
-      <h1 className="font-semibold">Mario Gueyraud</h1>
-      <h2 className="font-light mt-1 text-gray-300">Senior Front-End & Design Engineer</h2>
-      <p className="mt-7 font-light text-gray-300">
-        I love building digital experiences that people love and find delightful. With a keen eye for great taste and a commitment to simplicity and modernist design, I strive to create interfaces that are not only beautiful but also functional and intuitive.
+      <h1
+        className="font-semibold animate-enter"
+        style={{ "--stagger": 1 } as CSSProperties}
+      >
+        Mario Gueyraud
+      </h1>
+      <h2
+        className="font-light mt-1 text-gray-300 animate-enter"
+        style={{ "--stagger": 2 } as CSSProperties}
+      >
+        Senior Front-End & Design Engineer
+      </h2>
+      <p
+        className="mt-7 font-light text-gray-300 animate-enter"
+        style={{ "--stagger": 3 } as CSSProperties}
+      >
+        I love building digital experiences that people love and find
+        delightful. With a keen eye for great taste and a commitment to
+        simplicity and modernist design, I strive to create interfaces that are
+        not only beautiful but also functional and intuitive.
       </p>
 
-      <h2 className="font-semibold mt-16">Experience</h2>
-      <div className="mt-8 flex flex-col gap-6">
+      <h2
+        className="font-semibold mt-16 animate-enter"
+        style={{ "--stagger": 4 } as CSSProperties}
+      >
+        Experience
+      </h2>
+      <div
+        className="mt-8 flex flex-col gap-6 animate-enter"
+        style={{ "--stagger": 5 } as CSSProperties}
+      >
         <div className="flex">
           <div className="flex-1">
             <p className="font-light text-gray-300">2024</p>
           </div>
           <div className="flex-1">
-            <p>Full Stack Engineer at <a className="underline" target="_blank" href="https://saagas.ai/">Saagas.ai</a></p>
+            <p>
+              Full Stack Engineer at{" "}
+              <a
+                className="underline"
+                target="_blank"
+                href="https://saagas.ai/"
+              >
+                Saagas.ai
+              </a>
+            </p>
           </div>
         </div>
         <div className="flex">
@@ -24,7 +58,16 @@ export default function Home() {
             <p className="font-light text-gray-300">2023</p>
           </div>
           <div className="flex-1">
-            <p>Senior Front-End Engineer at <a className="underline" target="_blank" href="https://tinloof.com/">Tinloof</a></p>
+            <p>
+              Senior Front-End Engineer at{" "}
+              <a
+                className="underline"
+                target="_blank"
+                href="https://tinloof.com/"
+              >
+                Tinloof
+              </a>
+            </p>
           </div>
         </div>
         <div className="flex">
@@ -32,7 +75,12 @@ export default function Home() {
             <p className="font-light text-gray-300">2023</p>
           </div>
           <div className="flex-1">
-            <p>Front-End Engineer at <a className="underline" target="_blank" href="https://belk.com/">Belk</a></p>
+            <p>
+              Front-End Engineer at{" "}
+              <a className="underline" target="_blank" href="https://belk.com/">
+                Belk
+              </a>
+            </p>
           </div>
         </div>
         <div className="flex">
@@ -40,7 +88,16 @@ export default function Home() {
             <p className="font-light text-gray-300">2022</p>
           </div>
           <div className="flex-1">
-            <p>Lead Front-End Engineer at <a className="underline" target="_blank" href="https://tonic3.com/">Tonic 3</a></p>
+            <p>
+              Lead Front-End Engineer at{" "}
+              <a
+                className="underline"
+                target="_blank"
+                href="https://tonic3.com/"
+              >
+                Tonic 3
+              </a>
+            </p>
           </div>
         </div>
         <div className="flex">
@@ -48,7 +105,16 @@ export default function Home() {
             <p className="font-light text-gray-300">2021</p>
           </div>
           <div className="flex-1">
-            <p>React.js & UI Developer at <a className="underline" target="_blank" href="http://teixido.co/">Teixido</a></p>
+            <p>
+              React.js & UI Developer at{" "}
+              <a
+                className="underline"
+                target="_blank"
+                href="http://teixido.co/"
+              >
+                Teixido
+              </a>
+            </p>
           </div>
         </div>
         <div className="flex">
@@ -56,24 +122,60 @@ export default function Home() {
             <p className="font-light text-gray-300">2020</p>
           </div>
           <div className="flex-1">
-            <p>Sofware Engineer Intern at <a className="underline" target="_blank" href="http://teixido.co/">Teixido</a></p>
+            <p>
+              Sofware Engineer Intern at{" "}
+              <a
+                className="underline"
+                target="_blank"
+                href="http://teixido.co/"
+              >
+                Teixido
+              </a>
+            </p>
           </div>
         </div>
       </div>
 
-      <h2 className="font-semibold mt-16">Lab</h2>
-      <p className="font-light mt-2 text-gray-300">
-        In my proccess of learning design engineering and crafting animations, 
-        I&apos;ve built this section to have a curated list of designs to showcase my skills, 
-        as well as learning them. I share my journey as a design engineer <Link href="/lab" className="underline">here</Link>.
+      <h2
+        className="font-semibold mt-16 animate-enter"
+        style={{ "--stagger": 6 } as CSSProperties}
+      >
+        Lab
+      </h2>
+      <p
+        className="font-light mt-2 text-gray-300 animate-enter"
+        style={{ "--stagger": 7 } as CSSProperties}
+      >
+        In my proccess of learning design engineering and crafting animations,
+        I&apos;ve built this section to have a curated list of designs to
+        showcase my skills, as well as learning them. I share my journey as a
+        design engineer{" "}
+        <Link href="/lab" className="underline">
+          here
+        </Link>
+        .
       </p>
 
-      <h2 className="font-semibold mt-16">Get in touch</h2>
-      <div className="flex gap-2 mt-4 font-light text-gray-300">
-        <a href="https://github.com/mgueyraud" target="_blank">Github</a>
-        <a href="https://twitter.com/mariogyd" target="_blank">Twitter</a>
-        <a href="https://www.linkedin.com/in/mariogyd/" target="_blank">Linkedin</a>
-        <a href="mailto:mgueyraud.junior@gmail.com" >Mail</a>
+      <h2
+        className="font-semibold mt-16 animate-enter"
+        style={{ "--stagger": 8 } as CSSProperties}
+      >
+        Get in touch
+      </h2>
+      <div
+        className="flex gap-2 mt-4 font-light text-gray-300 animate-enter"
+        style={{ "--stagger": 8 } as CSSProperties}
+      >
+        <a href="https://github.com/mgueyraud" target="_blank">
+          Github
+        </a>
+        <a href="https://twitter.com/mariogyd" target="_blank">
+          Twitter
+        </a>
+        <a href="https://www.linkedin.com/in/mariogyd/" target="_blank">
+          Linkedin
+        </a>
+        <a href="mailto:mgueyraud.junior@gmail.com">Mail</a>
       </div>
     </>
   );
