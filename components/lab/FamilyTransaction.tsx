c44c652de051af6d5be5a5757d933f4aec2e7c58
mgueyraud <mgueyraud.junior@gmail.com>
2024-07-22 13:04:19 -0400

    Fixed flickering issue

--- components/lab/FamilyTransaction.tsx
+++ components/lab/FamilyTransaction.tsx
@@ -42,12 +42,13 @@ export default function FamilyTransaction() {
               }
             )}
           >
-            <AnimatePresence initial={false} mode="popLayout" custom={status}>
+            <AnimatePresence initial={false} mode="popLayout">
               <motion.div
-                key={`icon-${status}`}
+                key={status}
                 initial={{ scale: 0.5, opacity: 0 }}
                 animate={{ scale: 1, opacity: 1 }}
                 exit={{ scale: 0.5, opacity: 0 }}
+                transition={{ duration: 0.3 }}
                 layout="position"
                 className="flex items-center"
               >
